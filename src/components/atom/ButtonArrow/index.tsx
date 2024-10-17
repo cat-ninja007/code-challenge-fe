24ef4e9409a48e17c5c60458981e75f67258ea67
cat-ninja007 <archiedesign12.2@gmail.com>
2024-10-17 16:35:30 +0700

    [COMPONENT] - [BOTH] - Styling Button Arrow

--- src/components/atom/ButtonArrow/index.tsx
+++ src/components/atom/ButtonArrow/index.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
 import Image from "next/image";
 import RightArrow from '../../../public/icons/utils-icon/right-arrow.png'
 
@@ -6,32 +7,81 @@ interface IButtonArrow {
   title: string;
 }
 
-const ButtonArrow:FC<IButtonArrow> = (props:IButtonArrow) => {
+const ButtonArrow: FC<IButtonArrow> = (props: IButtonArrow) => {
   const { title } = props;
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMouseDown = () => {
+    setIsActive(true); // Saat tombol ditekan
+  };
+
+  const handleMouseUp = () => {
+    setIsActive(false); // Saat tombol dilepaskan (kembali ke gradien)
+  };
 
   return (
-    <>
-      {/* <div className="button-container w-full"> */}
-        <button className="
-          w-full 
-          h-14 
-          bg-gradient-to-r 
-          from-courtney-dark-yellow 
-          to-courtney-light-yellow 
-          text-courtney-white 
-          flex 
-          justify-center 
-          items-center
-          rounded-lg
-          font-bold
-          hover:bg-courtney-dark-yellow
-          ">
-          {title}
-          <Image src={RightArrow} alt="right-arrow" className="relative -right-3"/>
+    <div
+      className={`
+        button-container 
+        w-full
+        flex 
+        justify-center
+        items-center
+        rounded-lg
+        font-bold
+        ${isActive ? "bg-orange-500" : "bg-gradient-to-r from-courtney-dark-yellow to-courtney-light-yellow"}
+      `}
+      onMouseEnter={() => setIsActive(true)}  
+      onMouseLeave={() => setIsActive(false)} 
+      onMouseDown={handleMouseDown}           
+      onMouseUp={handleMouseUp}               
+      onFocus={() => setIsActive(true)}       
+      onBlur={() => setIsActive(false)}       
+    >
+      <button className="w-full h-14 text-courtney-white flex justify-center items-center">
+        {title}
+        <Image src={RightArrow} alt="right-arrow" className="relative -right-3"/>
         </button>
-      {/* </div> */}
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default ButtonArrow
+export default ButtonArrow;
+
+// import { FC } from "react";
+// import Image from "next/image";
+// import RightArrow from '../../../public/icons/utils-icon/right-arrow.png'
+
+// interface IButtonArrow {
+//   title: string;
+// }
+
+// const ButtonArrow:FC<IButtonArrow> = (props:IButtonArrow) => {
+//   const { title } = props;
+
+//   return (
+//     <>
+//       {/* <div className="button-container w-full"> */}
+//         <button className="
+//           w-full 
+//           h-14 
+//           bg-gradient-to-r 
+//           from-courtney-dark-yellow 
+//           to-courtney-light-yellow 
+//           text-courtney-white 
+//           flex 
+//           justify-center 
+//           items-center
+//           rounded-lg
+//           font-bold
+//           hover:bg-courtney-dark-yellow
+//           ">
+//           {title}
+//           <Image src={RightArrow} alt="right-arrow" className="relative -right-3"/>
+//         </button>
+//       {/* </div> */}
+//     </>
+//   )
+// }
+
+// export default ButtonArrow
