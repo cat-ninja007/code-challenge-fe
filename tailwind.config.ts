780edfd38b149961c8fa4992203efa073373c37f
cat-ninja007 <archiedesign12.2@gmail.com>
2024-10-17 15:24:39 +0700

    [ALL] - Setup Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,15 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'courtney-dark-blue': '#16205F',
+        'courtney-dark-yellow': '#FF9900',
+        'courtney-light-yellow': '#F2CB00',
+        'courtney-bright-yellow': '#FFF856',
+        'courtney-soft-grey': '#F9F9F9',
+        'courtney-normal-grey': '#E1E1E1',
+        'courtney-dark-grey': '#9F9F9F',
+        'courtney-white': '#FFFFFF',
+        'courtney-soft-blue': '#C5E0FF'
       },
     },
   },
