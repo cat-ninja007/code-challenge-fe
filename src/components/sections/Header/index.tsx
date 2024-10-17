d70b2c0427ca07f0fa2771f19f4cf34b448bc6a8
cat-ninja007 <archiedesign12.2@gmail.com>
2024-10-17 15:41:23 +0700

    [HEADER] - [MOBILE] - Finish Slicing

---
+++ src/components/sections/Header/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import Image from "next/image";
+
+import CourtneyLogo from '../../../public/images/courtney-logo.png'
+import BurgerMenu from '../../../public/icons/utils-icon/burger-menu.png'
+
+const Header:FC = () => {
+  return (
+    <>
+      <header>
+        <div className="header-section px-4 h-20 flex justify-between items-center border-2 border-courtney-dark-blue">
+          <div className="header-image-container w-36">
+            <Image src={CourtneyLogo} alt="courtney-logo" />
+          </div>
+
+          <div className="header-burger-menu">
+            <Image src={BurgerMenu} alt="burger-menu" />
+          </div>
+
+
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header;
