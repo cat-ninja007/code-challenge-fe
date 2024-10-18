7470f96f76d6c2290b8ba6f0e6d6a312b4fc4d1e
cat-ninja007 <archiedesign12.2@gmail.com>
2024-10-18 17:46:30 +0700

    [PAGE] - [MOBILE] - Mobile Temporary Finish Because The Dev Faint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 import { FC } from "react";
 
-import Header from "@/components/sections/Header";
-import Jumbotron from "@/components/sections/Jumbotron";
-import Solutions from "@/components/sections/Solutions";
-import HowToStart from "@/components/sections/HowToStart";
-import Expertise from "@/components/sections/Expertise";
-import Specialized from "@/components/sections/Specialized";
-import Footer from "@/components/sections/Footer";
-
 import Home from "./home/page";
-import About from "./about/page";
-import Service from "./service/page";
-import Team from "./team/page";
+
 
 const App:FC = () => {
   return (
